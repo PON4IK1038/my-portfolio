f971c13b6f08f3f467a20df1c569afc9747b75a1
PON4IK1038 <117180653+PON4IK1038@users.noreply.github.com>
2023-03-07 16:18:07 +0300

    Add files via upload

---
+++ portfolio-web-main/js/app.js
@@ -0,0 +1,32 @@
+const header = document.querySelector("header");
+
+function stickyNavbar(){
+	header.classList.toggle("scrolled", window.pageYOffset > 0)
+}
+
+stickyNavbar();
+
+window.addEventListener("scroll", stickyNavbar);
+
+
+/* --------------- Grab elements from DOM --------------- */
+
+/* --------------- Sticky Navbar --------------- */
+
+/* --------------- Reveal Animation --------------- */
+
+/* --------------- Skills Progress Bar Animation --------------- */
+
+/* --------------- Services Counter Animation --------------- */
+
+/* --------------- Portfolio Filter Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Change Active Link On Scroll --------------- */
+
+/* --------------- Change Page Theme --------------- */
+
+/* --------------- Open & Close Navbar Menu --------------- */
